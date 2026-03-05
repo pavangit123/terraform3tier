151732b86a2c8f61c3d5804b70e60a61e19b2678
Durga Pavan Chennam <pavan.chennam@appsassoc.com>
2026-03-05 16:00:47 +0530

    changed region

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "my-terraform-state-bucket" (edit as per requirement)
     key            = "3tier/terraform.tfstate"
-    region         = "ap-south-1" (edit as per requirement)
+    region         = "us-east-1" (edit as per requirement)
     dynamodb_table = "terraform-lock-table" (edit as per requirement)
     encrypt        = true
   }
